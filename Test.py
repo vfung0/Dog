90cee53aed013c93dfc639695312724fdedb3089
vfung0 <vfung@student.ccc.edu>
2024-04-26 16:35:13 -0500

    Hello World

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
-1. Showcase Your Dance Moves! 
+# 1. Showcase Your Dance Moves! 
 
-Task 1: Code Correction 
+# Task 1: Code Correction 
 
 weather = "sunny" 
  
@@ -10,15 +10,13 @@ else:
     print("Take an umbrella!") 
 
  
-
- 
-
-Task 2: Your Mood Today 
+# Task 2: Your Mood Today 
 
 # Ask the user how they feel today 
 user_feeling = input("How are you feeling today? ") 
  
 # Check the user's response 
+
 if user_feeling.lower() == "happy": 
     print("That's great to hear!") 
 elif user_feeling.lower() == "sad": 
@@ -27,10 +25,9 @@ else:
     print("I'm not sure how you're feeling, but I'm here to chat!") 
 
  
+# 2. Python Naming Convention Practice 
 
-2. Python Naming Convention Practice 
-
-Task 1: Code Correction 
+# Task 1: Code Correction 
 
 pi_value = 3.14 
 user_age = 25 
@@ -38,18 +35,9 @@ user_location = "New York"
 MAX_LIMIT = 1000 
 
  
+# 3. Python Data Types and type() Function 
 
- 
-
- 
-
- 
-
- 
-
-3. Python Data Types and type() Function 
-
-Task 1: Code Correction 
+# Task 1: Code Correction 
 
 variable_a = "Hello, World!" 
 variable_b = 23 
@@ -62,10 +50,9 @@ print("Data type of variable_c:", type(variable_c))
 print("Data type of variable_d:", type(variable_d)) 
 
  
+# 4. Arithmetic Operations in Daily Life 
 
-4. Arithmetic Operations in Daily Life 
-
-Task 1: Grocery Store Math 
+# Task 1: Grocery Store Math 
 
 # Given prices of grocery items 
 price_bread = 2.00 
@@ -80,9 +67,7 @@ print(f"The total cost of buying bread, peanut butter, and jelly is ${total_cost
 
  
 
- 
-
-Task 2: Bank Interest 
+# Task 2: Bank Interest 
 
 # Given initial amount and interest rate 
 initial_amount = 10000 
@@ -95,10 +80,8 @@ total_amount_after_one_year = initial_amount * (1 + interest_rate / 100)
 print(f"The total amount after one year at {interest_rate}% interest rate is ${total_amount_after_one_year:.2f}") 
 
  
-
-5. Understanding Assignments and Comparisons 
-
-Task 1: Value Swapping 
+# 5. Understanding Assignments and Comparisons 
+# Task 1: Value Swapping 
 
 # Given two numbers 
 a = 10 
@@ -108,28 +91,25 @@ b = 18
 a, b = b, a 
  
 # Compare the swapped values 
+
 if a == 18 and b == 10: 
     print("Values have been successfully swapped.") 
 else: 
     print("Values have not been swapped correctly.") 
 
  
-
-6. Exploring Logical Operations and Precedence 
-
- 
+# 6. Exploring Logical Operations and Precedence 
 
 # Calculate the expression normally 
 result_normal = 1 + 2 * 2 - 6 / 3 
 print(f"Result (Normal): {result_normal}") 
  
 # Calculate the expression with parentheses 
+
 result_parentheses = (1 + (2 * 2)) - (6 / 3) 
 print(f"Result (Parentheses): {result_parentheses}") 
 
- 
-
-Task 2: Mix and Match 
+# Task 2: Mix and Match 
 
 # Calculate the expression 
 result = (6 + 3) > 10 or (7 + 7) == 14 
